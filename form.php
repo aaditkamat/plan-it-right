469b37593e1865c38ecfecd2f941c3b86a03fe44
Aadit Kamat <aadit.k12@gmail.com>
2018-07-17 11:37:37 +0800

    Add sample php file and made changes to form styling

---
+++ form.php
@@ -0,0 +1,14 @@
+<html>
+    <body>
+        City:  <?php echo $_POST["City"]; ?>
+        <br>
+        Country: <?php echo $_POST["Country"]; ?>
+        <br>
+        Departure Date/Time: <?php echo $_POST["Departure"]; ?>
+        <br>
+        Return Date/Time: <?php echo $_POST["Return"]; ?>
+        <br>
+        Number of guests/passengers: <?php echo $_POST["Guests"]; ?>
+        <br>
+    </body>
+</html>
