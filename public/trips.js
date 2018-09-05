64bf7ef98c16eaefc7a53c7292ed7417b207aa6b
Aadit Kamat <aadit.k12@gmail.com>
2018-09-05 18:12:16 +0800

    Fix itinerary selection bug to select correct saved trip
    Signed-off-by: aaditkamat <aadit.k12@gmail.com>

--- public/trips.js
+++ public/trips.js
@@ -6,12 +6,13 @@ firebase.auth().onAuthStateChanged((user) => {
         let ctr = 1;
         for (value in data) {
             let detail = data[value];
+            detail.ctr = ctr;
             const startTime = detail.departure,
                 endTime = moment(moment(startTime).format("DD/MM/YYYY")).add(detail.lengthOfTrip - 1, "days").format("DD/MM/YYYY");
             const duration = `${detail.lengthOfTrip} days in ${detail.city}`, dateSpan = `${startTime} - ${endTime}`;
             const imageSrc = `images/destinations/${detail.city}.jpg`;
             if (ctr === 1) {
-                document.querySelector('img.background').src = imageSrc;
+                document.querySelector('.destination-image').src = imageSrc;
                 document.querySelector('div.duration').innerText = duration;
                 document.querySelector('div.date-span').innerText = dateSpan;
             }
@@ -25,5 +26,25 @@ firebase.auth().onAuthStateChanged((user) => {
             }
             ctr++;
         }
+        ctr = 1;
+        let arr = [];
+        for (let image of document.querySelectorAll('.destination-image')) {
+            arr.push({ctr: ctr++, image: image});
+            image.onclick = () => {
+                sessionStorage.clear();
+                for (value in data) {
+                    let detail = data[value];
+                    for (let item of  arr) {
+                        console.log(item.ctr + " " + detail.ctr);
+                        if (item.image === image && item.ctr === detail.ctr) {
+                            sessionStorage.setItem('formOptions', JSON.stringify(detail));
+                            break;
+                        }
+                    }
+                }
+                window.open('result-copy.html');
+            }
+        }
+        console.log(arr);
     });
 });
