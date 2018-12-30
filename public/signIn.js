f3f6dcea84fe88496ffd39e16343004818495106
Aadit Kamat <aadit.k12@gmail.com>
2018-12-30 09:53:54 +0800

    Remve unnecessary debugging statements

--- public/signIn.js
+++ public/signIn.js
@@ -9,22 +9,12 @@ var signInProcess = (event) => firebase.auth().signInWithPopup(event.data.provid
     setTimeout(() => {
         window.open('form.html', '_self');
     }, 2000);
-    console.log(result);
-    var user = result.user;
-    console.log(user.displayName + " logged into Plan It Right via " + event.data.accountType);
 }).catch(function (error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-    console.log(errorCode);
-    console.log(errorMessage);
-    console.log(email);
-    console.log(credential);
+  const errorMessage = error.message;
+  let errorMessageObject = JSON.parse(errorMessage);
+  errorMessageObject = errorMessageObject.error;
+  console.error(error.code);
+  console.error(JSON.stringify(errorMessageObject, null, "\t"));
 });
 
 /**
